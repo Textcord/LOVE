ccc41ddf6ac3cc6f2bc75780cad60bd42beb3b2c
Dev_Nergis <78343032+Dev-Nergis@users.noreply.github.com>
2022-05-18 21:59:30 +0900

    현1타

---
+++ Main/デレ/ヤンデレ/LOVE.py
@@ -0,0 +1,15 @@
+# LOVE
+
+from time import sleep
+
+name = input("\033[1;32m\n 이름 : ") 
+
+print("\033[1;31m")
+
+while True:
+    print(f"{name} 사랑해줘♥")
+    sleep(0.5)
+    print(f"{name} 사랑해줄래요?")
+    sleep(0.5)
+    print(f"{name} ")
+    print("\n")
