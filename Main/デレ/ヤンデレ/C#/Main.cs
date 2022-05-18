805e3f1a8e2ebf9a715a081e164d6d32f947b65b
NyongRyong <62173348+NyongRyong@users.noreply.github.com>
2022-05-18 23:56:53 +0900

    Update Main.cs

--- Main/デレ/ヤンデレ/C#/Main.cs
+++ Main/デレ/ヤンデレ/C#/Main.cs
@@ -5,5 +5,5 @@ while (true)
     Console.WriteLine(String.Format("{0} 사랑해줘❤", name));
     Thread.Sleep(500);
     Console.WriteLine(String.Format("{0} 사랑해줄래요?", name));
-    Thread.Sleep(1000);
+    Thread.Sleep(500);
 }
