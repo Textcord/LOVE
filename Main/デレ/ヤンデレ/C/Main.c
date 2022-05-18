276630a25b059b777ed555c68dffb76677a47f2a
NyongRyong <62173348+NyongRyong@users.noreply.github.com>
2022-05-18 23:28:58 +0900

    Create Main.c

---
+++ Main/デレ/ヤンデレ/C/Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <synchapi.h>
+
+int main() {
+    char name;
+    printf("이름 : ");
+    scanf("%c", &name);
+    while (1) {
+        printf("%c 사랑해줘❤\n", name);
+        Sleep(500);
+        printf("%c 사랑해줄래요?\n", name);
+        Sleep(1000);
+    }
+}
