afbd9b725827baf5d98f279517af70d7202f1bde
NyongRyong <62173348+NyongRyong@users.noreply.github.com>
2022-05-18 23:02:29 +0900

    Create Main.java

---
+++ Main/デレ/ヤンデレ/JAVA/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+        while (true) {
+            System.out.printf("%s 사랑해줘❤\n",name);
+            Thread.sleep(500);
+            System.out.printf("%s 사랑해줄래요?\n",name);
+            Thread.sleep(1000);
+        }
+    }
+}
