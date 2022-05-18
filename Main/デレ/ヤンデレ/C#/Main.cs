1a1615bc668feec9a1e09bce5d54cc1d85bba906
NyongRyong <62173348+NyongRyong@users.noreply.github.com>
2022-05-18 23:03:46 +0900

    Create Main.cs

---
+++ Main/デレ/ヤンデレ/C#/Main.cs
@@ -0,0 +1,9 @@
+Console.Write("이름 : ");
+string name = Console.ReadLine();
+while (true)
+{
+    Console.WriteLine(String.Format("{0} 사랑해줘❤", name));
+    Thread.Sleep(500);
+    Console.WriteLine(String.Format("{0} 사랑해줄래요?", name));
+    Thread.Sleep(1000);
+}
